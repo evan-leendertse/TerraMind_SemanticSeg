a867e74a5a12eb0d37ec86cc3505911969204109
evan-leendertse <evanleendertse@gmail.com>
2025-11-14 11:50:51 -0600

    Add files via upload

--- Test.py
+++ Test.py
@@ -3,6 +3,8 @@ import os
 from torch.utils.data import DataLoader
 import torch
 from collections import defaultdict
+import rasterio as rio
+import numpy as np
 import hydra
 from omegaconf import DictConfig
 from torch.utils.tensorboard import SummaryWriter
@@ -17,39 +19,40 @@ from DataLoader import TestLoader
 
 
 # Hydra sets configs outside of this file so we can edit params without touching this file
-@hydra.main(version_base = "1.2", config_path = "configs" , config_name = 'config_test') #Will need to be at least version 1.2 for Hydra sweeping
+@hydra.main(version_base = "1.2", config_path = "configs/test" , config_name = 'config') #Will need to be at least version 1.2 for Hydra sweeping
 def main(cfg: DictConfig):
     device = "cuda" if torch.cuda.is_available() else "cpu" # If cuda env set up correctly and using GPU, this will speed up runs massively
 
     # set location for outputs of results
     hc = HydraConfig.get()
-    output_dir = hc['run']['dir']
+    dir, subdir = hc['sweep']['dir'], hc['sweep']['subdir']
+    output_dir = os.path.join(dir, subdir)
+    print(output_dir)
     writer= SummaryWriter(log_dir = output_dir) 
 
     # Copy both the checkpoint and original configs file from trained model to output folder
     checkpoint_copy = os.path.join(output_dir, "model_checkpoint.pt")
-    config_copy = os.path.join(output_dir, "model_configs.pt")
-    shutil.copy(cfg.paths_.trained_model, checkpoint_copy)
-    shutil.copy(cfg.paths_.copy_configs, config_copy)
+    # config_copy = os.path.join(output_dir, "model_configs.pt")
+    shutil.copy(cfg.paths.trained_model, checkpoint_copy)
+    # shutil.copy(cfg.paths.copy_configs, config_copy)
 
 
     # prepare modalities paths from configs for data loader
     test_modalities = {
         name: (paths.before, paths.after)
-        for name, paths in cfg.paths_.modalities.items()}
+        for name, paths in cfg.paths.modalities.items()}
 
     # Load in desired data using test loader
     test_data = TestLoader(
         modalities = test_modalities,
-        label_dir= cfg.paths_.label_dir,
+        label_dir= cfg.paths.label_dir,
         patch_size = cfg.model.patch_size, 
-        stride = cfg.model.stride,
-        add_random_offset = cfg.model.add_random_offset)
+        stride = cfg.model.stride)
     #torch batch loader
-    test_dataloader = DataLoader(test_data, num_workers = cfg.model.num_workers)
+    test_dataloader = DataLoader(test_data, batch_size = None, num_workers = cfg.model.num_workers)
 
     # load in decoder + encoder based on trained model path provided
-    checkpoint_path = cfg.paths_.trained_model
+    checkpoint_path = cfg.paths.trained_model
     checkpoint = torch.load(checkpoint_path, map_location=device)
 
     encoder = TerraMindEncoder(version = "terramind_v1_base", 
@@ -69,9 +72,10 @@ def main(cfg: DictConfig):
 
     # Set dicts which will help us reconstruct original image from patches
     padding = {}
+    metas = {}
     tile_reconstruction = defaultdict(list)
     with torch.no_grad():
-        for x , y, (i, coord_y, coord_x), (pad_left, pad_right, pad_top, pad_bottom) in test_dataloader:
+        for x , y, (i, coord_y, coord_x), (pad_left, pad_right, pad_top, pad_bottom), meta in test_dataloader:
             # Use helper function on x output (dictionary) to move to device
             x = move_to_device(x, device)
 
@@ -89,6 +93,9 @@ def main(cfg: DictConfig):
             if idx not in padding:
                 padding[idx] = tuple(p.item() if torch.is_tensor(p) else int(p)
                                     for p in (pad_left, pad_right, pad_top, pad_bottom))
+            
+            if idx not in metas:
+                metas[idx] = meta
 
 
     # --------------- Taking information patch estimates/originals and recreating the entire images------------------ #
@@ -138,6 +145,43 @@ def main(cfg: DictConfig):
 
         # Zero-out predictions wherever ground truth is 0
         image_tiles_pred[idx] = torch.where(real == 0, torch.zeros_like(pred), pred)
+
+
+    # Save as geotiffs so we can use them later through google earth etc
+    tiff_dir = os.path.join(output_dir, "geotiffs")
+    os.makedirs(tiff_dir, exist_ok=True)
+
+    for idx, pred in image_tiles_pred.items():
+        meta = metas[idx]
+        meta_out = meta.copy()
+        meta_out.update({
+            "driver": "GTiff",
+            "height": pred.shape[0],
+            "width": pred.shape[1],
+            "count": 1,
+            "dtype": "uint8",
+            "nodata": 0
+
+        })
+
+        # Convert tensor → numpy array
+        pred_np = pred.cpu().numpy().astype(np.uint8)
+
+        # Output path
+        tiff_path = os.path.join(tiff_dir, f"predicted_map_{idx}_colored.tif")
+
+        # Define color map for google earth
+        color_table = {
+            0: (0, 0, 0, 255), #background)
+            1: (0, 255, 0, 255), #healthy
+            2: (255, 0, 0, 255) } #damaged
+
+        # Save colorized GeoTIFF
+        with rio.open(tiff_path, "w", **meta_out) as dst:
+            dst.write(pred_np, 1)
+            dst.write_colormap(1, color_table)
+
+
 
     # calculate and print metrics on an image tile level
     test_metrics = calc_test_metrics(image_tiles_pred, image_tiles_true, ignore_index=0, positive_class=2, negative_class=1)
